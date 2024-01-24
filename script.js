12d384a96fd18dbad0f47c2690eabfae70a10b2b
DanielB566 <Danib7382@gmail.com>
2024-01-24 11:56:02 -0500

    Finished JS

---
+++ script.js
@@ -0,0 +1,29 @@
+function buttonone(){
+    document.getElementById('imgchange').src = "testimg.png" // changes image
+    document.getElementById('titlechange').textContent = "words" // changes title
+    document.getElementById('textchange').textContent = "words" // changes text
+}
+
+function buttontwo(){
+    document.getElementById('imgchange').src = "testimg.png"
+    document.getElementById('titlechange').textContent = "More words"
+    document.getElementById('textchange').textContent = "words"
+}
+
+function buttonthree(){
+    document.getElementById('imgchange').src = "testimg.png"
+    document.getElementById('titlechange').textContent = "words roewrs owrosd"
+    document.getElementById('textchange').textContent = "words"
+}
+
+function buttonfour(){
+    document.getElementById('imgchange').src = "testimg.png"
+    document.getElementById('titlechange').textContent = "wrodsd wrosd wrosd"
+    document.getElementById('textchange').textContent = "words"
+}
+
+function buttonfive(){
+    document.getElementById('imgchange').src = "testimg.png"
+    document.getElementById('titlechange').textContent = "wrsdaisdisadasdhasdhas"
+    document.getElementById('textchange').textContent = "words"
+}
