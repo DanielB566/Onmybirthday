82af95a1bfc7448e957f504408144c150332e201
DanielB566 <Danib7382@gmail.com>
2024-01-31 09:26:46 -0500

    Final commit added footer and alt text

--- script.js
+++ script.js
@@ -3,35 +3,40 @@ function buttonone(){
     document.getElementById('titlechange').textContent = "Julius Caesar assassination" // changes title
     document.getElementById('textchange').textContent = "On March 15th 44 BC Julius Caesar is stabbed to death in Rome, Italy." // changes text
     document.getElementById('imgchange').alt = "A picture of Julius Caesar being assassinated."
+    
 }
 
 function buttontwo(){
     document.getElementById('imgchange').src = "imgs/andrewjackson.png"
     document.getElementById('titlechange').textContent = "Andrew Jackson "
     document.getElementById('textchange').textContent = "Andrew Jackson was born on march 15th 1762."
+    document.getElementById('imgchange').alt = "Picture of Andrew Jackson"
 }
 
 function buttonthree(){
     document.getElementById('imgchange').src = "imgs/maine.png"
     document.getElementById('titlechange').textContent = "Maine "
     document.getElementById('textchange').textContent = "In 1820 Maine becomes a US state."
+    document.getElementById('imgchange').alt = "Picture of the flag of Maine."
 }
 
 function buttonfour(){
     document.getElementById('imgchange').src = "imgs/Wilt.png"
     document.getElementById('titlechange').textContent = "Wilt Chamberlain"
     document.getElementById('textchange').textContent = "In 1962 Wilt Chamberlain is 1st to score 4,000 pts in an NBA season."
+    document.getElementById('imgchange').alt = "Picture of a newspaper talking about Wilt Chamberlain getting 4,000 points in the NBA."
 }
 
 function buttonfive(){
     document.getElementById('imgchange').src = "imgs/firstdomain.png"
     document.getElementById('titlechange').textContent = ".com website"
     document.getElementById('textchange').textContent = "In 1985 The very first .com domain was registered."
+    document.getElementById('imgchange').alt = "Picture of the first .com website."
 }
 
 function buttondefault(){
     document.getElementById('imgchange').src = "imgs/March15th.png"
     document.getElementById('titlechange').textContent = "Things that happened on my birthday."
     document.getElementById('textchange').textContent = "Click on the buttons below to see somethings that happened on my birthday."
-
+    document.getElementById('imgchange').alt = "Picture of a pin on the date March 15th."
 }
